dcf602c8c119a5fbf6877e3d9191dbb26c6b1e2c
Andreas Nylin <andreas.nylin@gmail.com>
2021-09-07 15:40:28 +0200

    Updated to version 2.0

--- kirbydance.js
+++ kirbydance.js
@@ -1,12 +1,37 @@
-(function() {
-	var a = "(>'.')>;<('.'<);^( '.' )^;v( '.' )v;<('.'<);(>'.')>;^( '.' )>;<( '.' )^;<( '.' )>;(>'.'<)".split(";"),
-		l = a.length - 1,
-		d = document,
-		t = document.title,
-		i = 0;
-	
-	window.setInterval(function() {
-		d.title = a[i++] + " " + t;
-		i > l&&(i = 0);
-	}, 1E3);
-})();
+/**
+   * KirbyDance.js
+   * Copyright (c) 2021 Andreas Nylin
+   * @version 2.0
+   * @license WTFPL
+   */
+
+function KirbyDance(element, mode = 'wrap', speed = 500) {
+    const animation = "(>'.')>;<('.'<);^( '.' )^;v( '.' )v;<('.'<);(>'.')>;^( '.' )>;<( '.' )^;<( '.' )>;(>'.'<)".split(";");
+    const animationLength = animation.length;
+    const originalText = element.textContent;
+    let index = Math.floor(Math.random() * animationLength);
+
+    function dance() {
+        const frame = animation[index];
+
+        if (mode === "append") {
+            element.innerText = `${originalText} ${frame}`;
+        }
+        else if (mode === "prepend") {
+            element.innerText = `${frame} ${originalText}`;
+        }
+        else {
+            element.innerText = `${frame} ${originalText} ${frame}`;
+        }
+
+        index++;
+
+        if (index >= animationLength) {
+            index = 0;
+        }
+
+        setTimeout(dance, speed);
+    }
+
+    dance();
+}
